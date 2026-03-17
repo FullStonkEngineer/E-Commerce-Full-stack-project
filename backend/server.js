a00c3b5a5a46631eeca514bd564e3daca02bf455
WiktorO3 <vociepka1@sheffield.ac.uk>
2026-03-17 11:26:13 +0100

    removed need for credentials in productions

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,14 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
